391d69a79b851583d1752d3e19c8ecb424fc8b4d
atharvtyagi-code <166939958+atharvtyagi-code@users.noreply.github.com>
2024-04-21 10:28:56 -0700

    Update main.py

--- Lesson-1/main.py
+++ Lesson-1/main.py
@@ -20,3 +20,28 @@ car = Car("Red", "GT321", "Diesel", 2024, "Mercedez_Benz")
 print(car.color)
 car.acceleration()
 car.brakes()
+print()
+
+
+
+class students():
+    def __init__(self, name, age, gender, birth_date, grade):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.birth_date = birth_date
+        self.grade = grade
+
+    def load(self):
+        print("Retrieving information...")
+
+print()
+Student = students("Luke", 14, "Male", "June 8th 2010", "8th grade")
+Student.load()
+print()
+print("Information retrieved:")
+print(Student.name)
+print(Student.age)
+print(Student.gender)
+print(Student.birth_date)
+print(Student.grade)
